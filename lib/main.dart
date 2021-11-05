5232fd34e638b4a36cf987a6122bde7a8b74d0c4
QClaire <38317525+Qclaire@users.noreply.github.com>
2021-11-05 01:20:30 +0000

    new style, buggy clipboard

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:clipboard/clipboard.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/services.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -31,9 +34,41 @@ class Dialer extends StatefulWidget {
   _DialerState createState() => _DialerState();
 }
 
-class _DialerState extends State<Dialer> {
+class _DialerState extends State<Dialer> with WidgetsBindingObserver {
   String _number;
   String msg = '';
+  String _clipboard='';
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    FlutterClipboard.paste().then((value)=>{
+      setState((){_clipboard = value;})
+    });
+  }
+
+  @override
+  void dispose(){
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state){
+    if(state==AppLifecycleState.inactive || state == AppLifecycleState.detached) return;
+    final isForeground =  state == AppLifecycleState.resumed;
+    print("\n\n$isForeground\n\n");
+    if (isForeground){
+      print("isResumed");
+      FlutterClipboard.paste().then((value)=>{
+      print("\n\nhi there$value\n\n"),
+        setState((){_clipboard = value;})
+      });
+    }
+  }
 
   _dialNumber()async{
     var urlToLaunch = _number.length == 10 && _number[0] == "0" ?
@@ -42,12 +77,16 @@ class _DialerState extends State<Dialer> {
 
     String error;
 
-    if (_number.length < 10) error = "You need 10 digits, you have ${_number.length}";
+    if (_number.length < 10) error = "${_number.length} digits; You need at least 10";
     else if (_number.length == 10 && _number[0] != "0") error = "Invalid number";
     else if(!await canLaunch(urlToLaunch)) error = "Sorry we couldn't open your number";
     if(error != null) setState(() { msg = error;});
     else launch(urlToLaunch);
     }
+
+
+
+
 
 
   @override
@@ -64,38 +103,52 @@ class _DialerState extends State<Dialer> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Text("Please input the number you wish to open in whatsApp"),
-            Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: TextFormField(
-                decoration: InputDecoration(icon: Icon(Icons.phone), contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
-                focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.green))),
-                style: TextStyle(fontSize: 30),
-                autofocus: true,
-                maxLength: 12,
-                textAlign: TextAlign.center,
+            Column(
+              children: [
+                Text("Type/Paste the number below", style: TextStyle(color: Colors.green, fontSize: 15, fontWeight: FontWeight.w900,),),
+                Text("We'll open it in whatsApp"),
+              ],
+            ),
 
-                keyboardType: TextInputType.phone,
-                onChanged:(number){
-                  setState(() {
-                    if(number[0] == "+") {
-                      msg = "No Symbols are allowed, please";
-                    }else {
-                      msg="";
-                      _number = number.replaceAll(" ", "");
-                    }
-                  });
-                },
+            _clipboard.length != 0 ? Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text("Copied: "),
+                Text("$_clipboard", style: TextStyle(color: Colors.green, fontSize: 15, fontWeight: FontWeight.w900,),),
+
+              ],
+            ):SizedBox.shrink(),
+
+
+            Padding(
+              padding: const EdgeInsets.only(left:18.0, right: 18.0, top: 18.0, bottom: 20,),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration( contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+                    focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.green))),
+                    style: TextStyle(fontSize: 30),
+                    textAlign: TextAlign.center,
+                    keyboardType: TextInputType.number,
+                    onChanged:(number){
+                      setState(() {
+                        if(number[0] == "+") {
+                          msg = "Symbols not allowed";
+                        }else {
+                          msg="";
+                          _number = number.replaceAll(" ", "");
+                        }
+                      });
+                    },
+                  ),
+                  Text(msg,style: TextStyle(fontSize: 16, color: Colors.redAccent),),
+                ],
               ),
             ),
-            Text(msg,style: TextStyle(fontSize: 16, color: Colors.redAccent),),
-
-            FlatButton(onPressed: _dialNumber,
-              color: Colors.green,
-              textColor: Colors.white,
-
-              padding: EdgeInsets.symmetric(vertical: 24, horizontal: 50),
-              child: Text("Open in WhatsApp",style: TextStyle(fontSize: 25),),
+            TextButton(onPressed: _dialNumber,
+              style: TextButton.styleFrom(backgroundColor: Colors.green, padding: EdgeInsets.symmetric(vertical: 20, horizontal:10),
+                  fixedSize: Size(MediaQuery.of(context).size.width*.90, MediaQuery.of(context).size.height*.10), elevation: 10.0),
+              child: Text("Open in WhatsApp",style: TextStyle(fontSize: 20, color: Colors.white),),
             )
           ],
         ),
